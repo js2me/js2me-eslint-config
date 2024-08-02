250cbb27dcda66986aa0fe8bd4407d5f7f411c66
Sergey Volkov <js2me@outlook.com>
2024-08-02 23:08:30 +0300

    initial commit

---
+++ configs/base.js
@@ -0,0 +1,108 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '.eslintrc.js', 'node_modules'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'prettier',
+    '@typescript-eslint'
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.js'],
+    },
+    'import/resolver': {
+      alias: {
+        extensions: ['.ts', '.js', '.json'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        printWidth: 120,
+        singleQuote: true,
+      },
+    ],
+    'import/no-unresolved': ['error'],
+    'no-duplicate-imports': 'error',
+    'no-trailing-spaces': 'error',
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxEOF: 0,
+      },
+    ],
+    'object-shorthand': 'error',
+    'no-empty-interface': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { classes: false, functions: false },
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-namespace': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external'],
+          ['builtin'],
+          ['internal'],
+          ['parent'],
+          ['sibling'],
+          ['object'],
+          ['index'],
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: [
+        "**/.eslintrc.js",
+      ],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+    {
+      files: [
+        "**/*.cjs",
+      ],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    }
+  ],
+}
---
+++ configs/react.js
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: [
+    require.resolve("./base.js"),
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    'import/resolver': {
+      alias: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: [],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-curly-brace-presence': [
+      'error',
+      { props: 'always', children: 'ignore' },
+    ],
+  },
+}
